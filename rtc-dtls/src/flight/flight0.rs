d277673de1eb87b6ac8988eef88e3a9975d1cc9a
nmammeri <nmammeri@gmail.com>
2026-09-13 19:57:27 +0200

    fix: reverted generate state.named_curve selection

--- rtc-dtls/src/flight/flight0.rs
+++ rtc-dtls/src/flight/flight0.rs
@@ -196,7 +196,7 @@ impl Flight for Flight0 {
         state.remote_epoch = 0;
 
         state.named_curve = DEFAULT_NAMED_CURVE;
-        if !state.is_cipher_suite_psk() {
+        if cfg.local_psk_callback.is_none() {
             state.named_curve = cfg.local_named_curves[0];
         }
         if let Err(error) = state.local_random.populate(cfg.provider().random()) {
